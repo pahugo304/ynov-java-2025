2a1f1fc7d3e27454f44cf2bbfa116286aca0f753
Pagola Hugo <hugo.pagola@ynov.com>
2025-03-03 17:01:19 +0100

    [ADD] For.java(loop)

---
+++ src/fr/ynov/java/easy/For.java
@@ -0,0 +1,29 @@
+package fr.ynov.java.easy;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+class While {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+class DoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
