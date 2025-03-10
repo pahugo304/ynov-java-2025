d092b5587b8ac82c47dc6a499092113acb0e52cf
Pagola Hugo <hugo.pagola@ynov.com>
2025-03-10 15:22:32 +0100

    [ADD] ScanMe.java

---
+++ src/fr/ynov/java/easy/ScanMe.java
@@ -0,0 +1,36 @@
+package fr.ynov.java.easy;
+
+import java.util.Scanner;
+
+public class ScanMe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        
+        System.out.println("Your name is " + name + " and you are " + age + " years old.");
+        
+        scanner.close();
+    }
+}
+
+class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (number % 2 == 0) {
+            System.out.println("The number " + number + " is even.");
+        } else {
+            System.out.println("The number " + number + " is odd.");
+        }
+        
+        scanner.close();
+    }
+}
